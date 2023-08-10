74d9c65a2ebd08046877af941410f5475b0f81ca
HanhTran1911 <tranhanh191103@gmail.com>
2023-08-10 10:31:47 +0700

    insert reponsive tablet

---
+++ js/slide_thitbi.js
@@ -0,0 +1,28 @@
+
+    let slideIndex1 = 1;
+    showSlides1(slideIndex1);
+
+    function plusSlides1(n) {
+        showSlides1(slideIndex1 += n);
+    }
+
+    function currentSlide1(n) {
+        showSlides1(slideIndex1 = n);
+    }
+
+
+    function showSlides1(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlide1");
+        let dots = document.getElementsByClassName("demo1");
+        if (slides && slides.length > 0 && n > 0 && n <= slides.length) {
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[n - 1].style.display = "block";
+            dots[n - 1].className += " active";
+        }
+    }
